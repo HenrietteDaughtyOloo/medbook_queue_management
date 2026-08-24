9fc891ac12b9b2b68a78038d9e5fb580f1658d80
Henriette Daughty <holoo@safaricom.co.ke>
2026-08-24 12:49:01 +0300

    feat(Http/Requests): implement FormRequests for input validation

---
+++ medbook_/backend/app/Http/Requests/StoreCustomerRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\Priority;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+
+class StoreCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'service' => ['required', 'string', 'max:255'],
+            'arrival_at' => ['required', 'date', 'before_or_equal:now'],
+            'original_priority' => ['required', Rule::enum(Priority::class)],
+        ];
+    }
+    
+    }
